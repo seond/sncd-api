a60f10a23583a2f60182c6f00f83ab061dc2e760
Seon Kim <playingnow@gmail.com>
2021-09-19 19:57:18 -0400

    Use @leetim25/strm package (#2)
    * Use @leetim25/strm package
    
    * Support message publishing to Strm

--- src/wss.ts
+++ src/wss.ts
@@ -1,7 +1,8 @@
 import { Server as WSServer } from 'ws';
-import { Strm } from 'strm-client-lib';
+import Strm from '@leetim25/strm';
+import { throttle } from 'lodash';
 
-import { STRM_API_URL, STRM_CS_URL } from './common/config';
+import { STRM_API_URL, STRM_CS_URL, STRM_PD_URL } from './common/config';
 import { getOneById as getDeckById } from './model/deck';
 import { parseUrlQueryParams } from './common/helpers';
 import { getStrmToken } from './common/strm';
@@ -28,7 +29,7 @@ wsServer.on('connection', async (ws, request, client) => {
 
     let conn = conns[deck.strmId];
     if (!conn) {
-        conn = await getNewStrmConnection(deck.strmId);
+        conn = await getNewStrmConnection(deck.strmId, deck.strmPatchKey);
         conn.onChange((doc, revisionNumber) => {
             // TODO: validate doc
             wsServer.clients.forEach(client => {
@@ -39,21 +40,56 @@ wsServer.on('connection', async (ws, request, client) => {
                         revisionNumber
                     }));
                 }
-            })
+            });
+            throttledUpdateDeck(deck, {
+                slides: doc.slides,
+                currentSlide: doc.currentSlide
+            });
+            deck.slides = doc.slides;
+            deck.currentSlide = doc.currentSlide;
         });
+        conn.init();
+        conn.connect();
+        conns[deck.strmId] = conn;
     }
+    
+    ws.on('message', message => {
+        const obj = JSON.parse(message.toString('utf-8'));
+        switch(obj.action) {
+            case 'next':
+                conn.put({
+                    slides: ws.deck.slides,
+                    currentSlide: ws.deck.currentSlide + 1
+                });
+                break;
+            case 'prev':
+                conn.put({
+                    slides: ws.deck.slides,
+                    currentSlide: ws.deck.currentSlide - 1
+                });
+                break;
+        }
+    });
 });
 
 export default wsServer;
 
-async function getNewStrmConnection(strmId: string) {
+async function getNewStrmConnection(strmId: string, strmActionKey: string) {
     const strmToken = await getStrmToken();
     const strmClient = new Strm(strmId, strmToken, {
         api_url: `${STRM_API_URL}/api/v1/docs`,
-        ws_url: STRM_CS_URL
+        ws_url: STRM_CS_URL,
+        pd_url: STRM_PD_URL,
+        action_key: strmActionKey
     });
-    strmClient.init();
-    strmClient.connect();
-
+    
     return strmClient;
 }
+
+function updateDeck(deck, data) {
+    deck.slides = data.slides;
+    deck.currentSlide = data.currentSlide;
+    deck.save();
+};
+
+const throttledUpdateDeck = throttle(updateDeck, 500);
